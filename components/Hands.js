0962a0c83f4adb4f7d579c9960307b92580a0306
Rachel Kim <yek99kr@gmal.com>
2022-06-02 16:37:06 -0400

    a

--- components/Hands.js
+++ components/Hands.js
@@ -1,7 +1,7 @@
 import { Suspense, useState, useEffect } from "react";
-import { Canvas, useFrame } from "@react-three/fiber";
+import { Canvas, useFrame, useThree } from "@react-three/fiber";
 import Hand from "./Hand";
-import { useTexture, Html } from "@react-three/drei";
+import { Text, useTexture, Html } from "@react-three/drei";
 import { Physics, useSphere, useBox, usePlane } from "@react-three/cannon";
 
 function Ball({ start, setStart }) {
@@ -69,16 +69,25 @@ function Paddle({ args = [5, 0.65, 0.6], start }) {
 
   return (
     <mesh ref={ref}>
-      <Html
+      {/* <Html
         transform
         position={[0, 0.05, 0.5]}
         className="transform relative pointer-events-none"
       >
         <div className="relative pointer-events-none">Submit</div>
-      </Html>
+      </Html> */}
       <boxGeometry args={args}></boxGeometry>
 
       <meshBasicMaterial color="white" toneMapped={false} />
+      <Text
+        color="black"
+        anchorX="center"
+        anchorY="middle"
+        position={[0, 0.05, 0.5]}
+        fontSize={0.37}
+      >
+        Submit
+      </Text>
     </mesh>
   );
 }
